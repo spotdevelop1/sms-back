ae2d253f335e28a9f26632486e0ba9126568fe01
CarlosBanda <c.banda07@hotmail.com>
2024-08-30 16:46:36 -0600

    add mysql and create users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
 @Module({
-  imports: [],
+  imports: [UsersModule,TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'Adm1nl1nux$pot',
+    database: 'sms-project',
+    entities: [__dirname + '/../**/*.entity.js'],
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
